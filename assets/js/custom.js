cefd1d7732cdceea2014351af1a9377185d71e5d
Leo-mst <ntloi088@gmail.com>
2016-07-08 14:16:40 +0700

    Updated Home

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -18,24 +18,31 @@ var mst = jQuery.noConflict();
 		});
 	});
 	mst(document).ready(function () {
-		mst('.menu-left-responsive').click(function () {
-			if (mst('.menu-left-container').css("left") == "-250px") {
-			  mst('.menu-left-container').animate({left: '0px'}, 150);
+		//right
+		mst('.menu-sidebar-right').click(function () {
+			if (mst('.menu-sidebar-container.right').css("right") == "-300px") {
+			  mst('.menu-sidebar-container.right').animate({right: '0px'}, 150);
+			  mst('.close-sidebar-btn').addClass('sidebar-active');
 			} 
 			else  {
-			  mst('.menu-left-container').animate({left: '-250px'}, 150); 
+			  mst('.menu-sidebar-container.right').animate({right: '-300px'}, 150); 
 			} 
 		});
 		mst(document).click(function(){
-			if(mst('.menu-left-container').css('left') == '0px' && mst('.menu-left-responsive').hasClass('on') ) {
-				mst('.menu-left-container').animate({left: '-250px'}, 150);
-				mst('.menu-left-responsive').toggleClass("on");
+			if(mst('.menu-sidebar-container.right').css('right') == '0px' && mst('.menu-sidebar-right').hasClass('on') ) {
+				mst('.menu-sidebar-container.right').animate({right: '-300px'}, 150);
+				mst('.menu-sidebar-right').toggleClass("on");
+				mst('.close-sidebar-btn').removeClass('sidebar-active');	
 			}
 		})
-		mst('.menu-left-responsive').click(function () {
-			mst(this).toggleClass("on"); 
-		}); // animation = -> x 
-	
+		/* mst('.menu-sidebar-right').click(function () {
+			mst(this).toggleClass("on");	
+		});  */
+		mst('#close_sidebar_btn').click(function () {
+			mst('.menu-sidebar-right').removeClass("on");
+			mst('.menu-sidebar-container.right').animate({right: '-300px'}, 150);
+			mst('.close-sidebar-btn').removeClass('sidebar-active');
+		}); 
 	});
 
 
